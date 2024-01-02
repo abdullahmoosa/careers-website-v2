75e72b423a92a200f97c450b73247244e3a3e7c9
Abdullah <amoosa@relisource.com>
2024-01-02 17:55:07 +0600

    Added app.py and Templates

---
+++ app.py
@@ -0,0 +1,12 @@
+# Module name must start with lowercase
+# To render a template, we can use the "render_template" function
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
