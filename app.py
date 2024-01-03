815b90beaa85cf85745eaf175c8dd4cb960a478e
Abdullah <amoosa@relisource.com>
2024-01-03 16:47:07 +0600

    Added job items

--- app.py
+++ app.py
@@ -4,9 +4,32 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+JOBS = [
+    {'id' : 1,
+     'title' : 'Data Analyst',
+     'location' : 'Bengaluru, India',
+     'Salary' : 'Rs. 10,00,000'
+    },
+    {'id' : 2,
+     'title' : 'Data Scientist',
+     'location' : 'Delhi, India',
+     'Salary' : 'Rs. 15,00,000'
+    },
+    {'id' : 3,
+     'title' : 'Frontend Developer',
+     'location' : 'Remote',
+     'Salary' : 'Rs. 10,00,000'
+    },
+    {'id' : 4,
+     'title' : 'Backend Developer',
+     'location' : 'San Francisco,USA',
+     'Salary' : '$ 120,000'
+    },
+]
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs = JOBS)
 
 
 if __name__ == '__main__':
