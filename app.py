61849fe260d7a9dd4d0dea72860dcffc9450850f
Abdullah <amoosa@relisource.com>
2024-01-05 17:17:18 +0600

    Implemented REST API to get job info from DB

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 # Module name must start with lowercase
 # To render a template, we can use the "render_template" function
+import json
 from flask import Flask, render_template, url_for,jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 app = Flask(__name__)
 
-JOBS =load_jobs_from_db()
 @app.route('/')
-def hello_world():
+def render_jobs():
+    JOBS =load_jobs_from_db()
     return render_template('home.html',jobs = JOBS)
 
 @app.route('/api/jobs')
 def list_jobs():
+    JOBS =load_jobs_from_db()
     return jsonify(JOBS)
 
+@app.route('/job/<id>')
+def get_job(id):
+    job = load_job_from_db(id)
+
+    if job:
+        print(f"Type of job is {type(job)}")
+        return jsonify(job)
+    else:
+        return jsonify({"error": f"Job with id = {id} not found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -25,14 +25,36 @@ class Table(Base):
 
 engine = create_engine(f"postgresql+psycopg2://{username}:{password}@{hostname}/{database}")
 Session = sessionmaker(bind=engine)
+
 def load_jobs_from_db():
     with Session() as read_session:
         # Query all records from the 'jobs' table
         results = read_session.query(Table).all()
-        print(f"Result type: {type(results)}")
+        print(f"Length of results: {len(results)}")
         print("Case 1: Existing Data - Read Operation")
         jobs = []
         for result in results:
             jobs.append(result.__dict__)
 
         return jobs
+    
+def load_job_from_db(id):
+    with Session() as read_session:
+        specific_job = read_session.query(Table).get(id)
+
+        if specific_job:
+            print(f"Information for Job with id = {id}")
+            job_data = {
+                "id": specific_job.id,
+                "title": specific_job.title,
+                "location": specific_job.location,
+                "salary": specific_job.salary,
+                "currency": specific_job.currency,
+                "responsibilities": specific_job.responsibilities,
+                "requirements": specific_job.requirements
+            }
+            return job_data
+        else:
+            print(f"Job with id = {id} not found")
+
+        return None
